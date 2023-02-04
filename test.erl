4885392948d6847cb4735a6705dbad44dfb0de3a
Безбородов, Д.Р <dbezborodov@gmail.com>
2023-02-05 01:49:41 +1030

    Update test.erl

--- test.erl
+++ test.erl
@@ -2,8 +2,7 @@
 -export([wc/1]).
 
 wc(InputText) ->
-    P = open_port({spawn, "wc"}, [stream, exit_status, use_stdio,
-                                  stderr_to_stdout, in, out, eof]),
+    P = open_port({spawn, "wc"}, [stream, use_stdio]),
     P ! {self(), {command, InputText}},
     erlang:port_control(P, 16#0112c000, []),
     receive {P, X} -> X end.
